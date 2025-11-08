ce10236c223630b6c9b8194c65485e5fe2c465db
levan <legushka06@gmail.com>
2025-11-08 09:50:19 +0700

    Added Cart class

---
+++ autoparts/cart.hpp
@@ -0,0 +1,19 @@
+#ifndef CART_HPP
+#define CART_HPP
+
+#include "autopart.hpp"
+#include <unordered_map>
+
+class Cart {
+private:
+    std::unordered_map<AutoPart*, int> items;
+    
+public:
+    void addItem(AutoPart* part, int qty = 1);
+    void removeItem(AutoPart* part);
+    double getTotal() const;
+    void clear();
+    size_t getItemCount() const;
+};
+
+#endif
